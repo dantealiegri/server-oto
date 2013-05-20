be4c790532ab8e9e483775c30cb1186b282d72ea
Drew Ogle <daogle@proquobooks.com>
2013-05-20 13:53:16 -0400

    added parsing testing for new client.

--- OtoServer/Omaha/Testing.cs
+++ OtoServer/Omaha/Testing.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Web;
 using System.Xml.Serialization;
 using System.IO;
+using System.Runtime.Serialization;
+using System.Xml;
 
 namespace OtoServer.Omaha
 {
@@ -12,12 +14,57 @@ namespace OtoServer.Omaha
 
         public static void Serialize()
         {
+            DataContractSerializer dcs_c_s = new DataContractSerializer( typeof(V3.OmahaClient));
             XmlSerializer client_serialize = new XmlSerializer( typeof(V3.OmahaClient));
             XmlSerializer server_serialize = new XmlSerializer( typeof(V3.OmahaClientResponse));
             StringWriter ssw = new StringWriter();
             StringWriter csw = new StringWriter();
             StringWriter sdsw = new StringWriter();
             StringWriter cdsw = new StringWriter();
+
+            StringWriter d_csw = new StringWriter();
+            XmlTextWriter xw = new XmlTextWriter(d_csw);
+            xw.Formatting = Formatting.Indented;
+
+            string client_xml =
+                @"<?xml version=""1.0"" encoding=""UTF-8""?>
+<request protocol=""3.0"" version=""1.3.23.0"" ismachine=""0"" sessionid=""{5FAD27D4-6BFA-4daa-A1B3-5A1F821FEE0F}"" userid=""{D0BBD725-742D-44ae-8D46-0231E881D58E}"" installsource=""scheduler"" testsource=""ossdev"" requestid=""{C8F6EDF3-B623-4ee6-B2DA-1D08A0B4C665}"">
+  <os platform=""win"" version=""6.1"" sp="""" arch=""x64""/>
+  <app appid=""{430FD4D0-B729-4F61-AA34-91526481799D}"" version=""1.3.23.0"" nextversion="""" lang=""en"" brand=""GGLS"" client=""someclientid"" installage=""39"">
+    <updatecheck/>
+    <ping r=""1""/>
+  </app>
+  <app appid=""{D0AB2EBC-931B-4013-9FEB-C9C4C2225C8C}"" version=""2.2.2.0"" nextversion="""" lang=""en"" brand=""GGLS"" client="""" installage=""6"">
+    <updatecheck/>
+    <ping r=""1""/>
+  </app>
+</request>";
+            String server_xml =
+                @"<?xml version=""1.0"" encoding=""UTF-8""?>
+<response protocol=""3.0"" server=""prod"">
+  <daystart elapsed_seconds=""56508""/>
+  <app appid=""{430FD4D0-B729-4F61-AA34-91526481799D}"" status=""ok"">
+    <updatecheck status=""noupdate""/>
+    <ping status=""ok""/>
+  </app>
+  <app appid=""{D0AB2EBC-931B-4013-9FEB-C9C4C2225C8C}"" status=""ok"">
+    <updatecheck status=""ok"">
+      <urls>
+        <url codebase=""http://cache.pack.google.com/edgedl/chrome/install/782.112/""/>
+      </urls>
+      <manifest version=""13.0.782.112"">
+        <packages>
+          <package hash=""VXriGUVI0TNqfLlU02vBel4Q3Zo="" name=""chrome_installer.exe"" required=""true"" size=""23963192""/>
+        </packages>
+        <actions>
+          <action arguments=""--do-not-launch-chrome"" event=""install"" run=""chrome_installer.exe""/>
+          <action version=""13.0.782.112"" event=""postinstall"" onsuccess=""exitsilentlyonlaunchcmd""/>
+        </actions>
+      </manifest>
+    </updatecheck>
+    <ping status=""ok""/>
+  </app>
+</response>";
 
             V3.OmahaClient sample_request = new V3.OmahaClient {
                 protocol = "3.0",
@@ -178,8 +225,10 @@ namespace OtoServer.Omaha
 
                 })
             };
-                                
 
+            XmlTextReader xtr = new XmlTextReader( new StringReader(client_xml));
+            dcs_c_s.WriteObject(xw, sample_request);
+            object o = dcs_c_s.ReadObject(xtr);
 
 
             client_serialize.Serialize(csw, sample_request);
